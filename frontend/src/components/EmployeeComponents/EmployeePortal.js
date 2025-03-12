f17978feef33e35d57bcd4236e27bdfe6c418491
Aeldahdahy <a.eldahdahy@outlook.com>
2025-03-12 19:42:36 +0200

    idea-0.1.82

--- frontend/src/components/EmployeeComponents/EmployeePortal.js
+++ frontend/src/components/EmployeeComponents/EmployeePortal.js
@@ -7,6 +7,10 @@ import EmployeeManageStaff from './EmployeeManageStaff';
 import EmployeeManageProjects from './EmployeeManageProjects';
 import EmployeeManageMessages from './EmployeeManageMessages';
 import EmployeeManageUsers from './EmployeeManageUsers';
+import EmployeeManageAd from './EmployeeManageAd';
+import EmployeeManageContract from './EmployeeManageContract';
+import EmployeeMobileWebContent from './EmployeeMobileWebContent';
+import EmployeeManageMeeting from './EmployeeManageMeeting';
 
 function EmployeePortal() {
   return (
@@ -26,12 +30,12 @@ function EmployeePortal() {
               <Route path="" element={<EmployeeMain />} />
               <Route path="/manageStaff" element={<EmployeeManageStaff />} />
               <Route path="/manageProject" element={<EmployeeManageProjects />} />
-              <Route path="/manageMeetingRequest" element={<h1>Meeting Requests</h1>} />
-              <Route path="/manageContractRequest" element={<h1>Contract Request</h1>} />
+              <Route path="/manageMeetingRequest" element={<EmployeeManageMeeting />} />
+              <Route path="/manageContractRequest" element={<EmployeeManageContract />} />
               <Route path="/manageMessages" element={<EmployeeManageMessages />} />
               <Route path="/manageUsers" element={<EmployeeManageUsers />} />
-              <Route path="/manageMobileWeb" element={<h1>Mobile Web</h1>} />
-              <Route path="/manageAd" element={<h1>Manage Ad</h1>} />
+              <Route path="/manageMobileWeb" element={<EmployeeMobileWebContent />} />
+              <Route path="/manageAd" element={<EmployeeManageAd />} />
             </Routes>
           </div>
         </div>

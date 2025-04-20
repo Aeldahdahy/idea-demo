fc890718b5cabaac85b50a5cccbd39ed3c2161ae
Aeldahdahy <a.eldahdahy@outlook.com>
2025-04-20 14:50:07 +0200

    idea-0.1.95

--- frontend/src/components/ClientComponents/ClientPortal.js
+++ frontend/src/components/ClientComponents/ClientPortal.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ClientInvestorHome from './ClientInvestorHome';
+import ClientEntreHome from './ClientEntreHome';
 
 function ClientPortal() {
+  const role = useSelector((state) => state.auth.role);
+
+
   return (
     <Routes>
-      <Route path="" element={<h1>Welcome to Client Portal</h1>} />
+      <Route
+        path="/"
+        element={role === "Investor" ? <ClientInvestorHome /> : <ClientEntreHome />}
+      />
     </Routes>
   );
 }
